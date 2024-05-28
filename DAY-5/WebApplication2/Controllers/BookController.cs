e496185de5547e9fba626429589cb0da2ec94e6a
Vikas A Adodariya <106027283+VIKASADODARIYA@users.noreply.github.com>
2024-05-28 11:00:32 +0530

    Add files via upload

---
+++ DAY-5/WebApplication2/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using DataLayer.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer;
+using ServiceLayer.Model;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BookController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBook()
+        {
+            var result = _bookService.GetAll();
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("GetBookById")]
+        public IActionResult GetBookById(int id)
+        {
+            var result = _bookService.GetById(id);
+            return Ok(result);
+        }
+
+        [Authorize(Roles ="Admin")]
+        [HttpPost]
+        public async Task<IActionResult> InsertBook(BookDetails book)
+        {
+            await _bookService.InsertBookDetails(book);
+            return Ok();
+        }
+    }
+}
